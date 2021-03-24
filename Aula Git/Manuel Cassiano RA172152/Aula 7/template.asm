484b59feca73750219042082c711b73c3a7dd275
Cassiano013 <cassianomanuel13@gmail.com>
2021-03-24 19:27:19 -0300

    rato do ak

---
+++ Aula Git/Manuel Cassiano RA172152/Aula 7/template.asm
@@ -0,0 +1,98 @@
+;  /===============================================================================\
+; | Projeto:  TÍTULO																|	
+; |																					|
+; | Autor: NOME DO ESTUDANTE AQUI													|
+; | Idioma: NOME DA LÍNGUA EM QUE O PROGRAMA ESTÁ ESCRITO E							|
+; |			O NOME DO COMPILADOR USADO PARA COMPILÁ-LO QUANDO FOI TESTADO			|
+; | Solução: NOME DA SUA SOLUÇÃO ATMEL STUDIO 7 (Template neste exemplo)			|
+; |																					|
+; | Projetos: AssemblerTargetTeamplate												|
+; |																					|
+; | Para compilar: Explique como compilar este programa	                            |
+; |																					|
+; |Exemplo: Para compilar este código, deverá ter o compilador AVRASM2.exe (windows)|
+; | no prompt de comando (MS-DOS):													|
+; |																					|
+; |      avrasm2.exe -fI -o "Teamplate.hex"											|
+; |						 -m "Teamplate.map"											|
+; | 					 -l "Teamplate.lss"											|	
+; |						 -e "Teamplate.eep"											|  
+; |						 -S "Teamplate.tmp"											|	  
+; |						 -W+ie  -d "Debug\Teamplate.obj"							|
+; |						 main.asm													|
+; |																					|
+; | Software: NOME E TÍTULO DA CLASSE PARA QUE ESTE PROGRAMA FOI ESCRITO			|
+; |																					|
+; | Versão do SO: SEU NÚMERO DE VERSÃO DO PROGRAMA DE COMPUTADOR					|
+; |																					|
+; | Plataforma: SEU PROGRAMA DE COMPUTADOR											|
+; |																					|
+; | Instrutor: NOME DO INSTRUTOR DO SEU CURSO										|
+; |																					|
+; | Data de vencimento: DATA E HORA EM QUE ESTE PROGRAMA É / DEVIA SER				|
+; | SUBMETIDO																		|
+; |																					|
+; + --------------------------------------------------------------------------------+
+; |																					|
+; | Descrição: DESCREVA O PROBLEMA DE QUE ESTE PROGRAMA FOI ESCRITO RESOLVER.		|
+; |																					|
+; | Entrada: DESCREVA A ENTRADA QUE O PROGRAMA EXIGE.								|
+; |																					|
+; | Saída: DESCREVA A SAÍDA QUE O PROGRAMA PRODUZ.									|
+; |																					|
+; | Algoritmo: ESBOÇO DA ABORDAGEM USADA PELO PROGRAMA PARA RESOLVER O PROBLEMA.	|
+; |																					|
+; | Recursos necessários não incluídos: DESCREVA AQUI QUALQUER REQUISITO DE			|
+; | A tarefa que o programa não tenta resolver.										|
+; |																					|
+; | Erros conhecidos: SE O PROGRAMA NÃO FUNCIONAR CORRETAMENTE EM ALGUNS			|
+; |					  SITUAÇÕES, DESCREVA AS SITUAÇÕES E PROBLEMAS AQUI.			|
+; |																					|
+;  \===============================================================================/
+
+
+.INCLUDE "atmega328p.inc"  ; seu arquivo principal de mapeamento das variáveis   
+.device		atmega328P
+.nolist
+.list
+
+;============
+;Declarações:
+
+
+
+.ORG 0x0000              ; instrução inicial, é onde se inicia a memória de programa
+    rjmp INICIO           ; O vetor de reinício: pula para “main”
+      ;***coloque suas variáveis e declarações aqui***   
+	;Exemplo: 
+	.def	temp	=r16
+ 
+INICIO:
+ldi temp, 0b11111111
+      out DDRB, temp
+
+      ldi temp, 0b11111111
+      out DDRC, temp
+
+      ldi temp, 0b11111111
+      out DDRD, temp
+      ;***código principal vai aqui, inicializações e etc...***
+LOOP:
+ ldi temp, 0b00001111
+      out PORTB, temp
+      ldi temp, 0b11110000
+      out PORTB, temp
+
+      ldi temp, 0b00001111
+      out PORTC,temp
+      ldi temp, 0b11110000
+      out PORTC, temp
+
+      ldi temp, 0b00001111
+      out PORTD, temp
+      ldi temp, 0b11110000
+      out PORTD, temp
+      ;*** rotinas de repetição ***
+   rjmp LOOP                 
+      ;***seus procedimentos de atuação finitas vão aqui aqui o  programa vai finalizar no final***
+.EXIT 
