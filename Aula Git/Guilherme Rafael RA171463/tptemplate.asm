6e6ae000998184db9b0ca680a5c828af5605888d
Guilherme-rgb <guilherme_rafael2011@hotmail.com>
2021-03-26 14:52:16 -0300

    ok AGORA tá certo plz

--- Aula Git/Guilherme Rafael RA171463/tptemplate.asm
+++ Aula Git/Guilherme Rafael RA171463/tptemplate.asm
@@ -12,11 +12,17 @@
  
 INICIO:
       ldi temp, 0b11111111
+	  out DDRB, temp
+      ldi temp, 0b11111111
 	  out DDRC, temp
 	  ldi temp, 0b11111111
 	  out DDRD, temp
 LOOP:
       ldi temp, 0b00001111
+	  out PORTB, temp
+	  ldi temp, 0b11110000
+	  out PORTB, temp
+	  ldi temp, 0b00001111
 	  out PORTC, temp
 	  ldi temp, 0b11110000
 	  out PORTC, temp
