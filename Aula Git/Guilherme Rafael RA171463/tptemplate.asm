9db5e836250320b5a36d97ea0311744d03061402
Guilherme-rgb <guilherme_rafael2011@hotmail.com>
2021-03-26 14:24:40 -0300

    acho que tá certo agora

---
+++ Aula Git/Guilherme Rafael RA171463/tptemplate.asm
@@ -0,0 +1,29 @@
+.INCLUDE "atmega328p.inc"   
+.device		atmega328P
+.nolist
+.list
+
+
+
+
+.ORG 0x0000   
+    rjmp INICIO   
+	.def	temp	=r16
+ 
+INICIO:
+      ldi temp, 0b11111111
+	  out DDRC, temp
+	  ldi temp, 0b11111111
+	  out DDRD, temp
+LOOP:
+      ldi temp, 0b00001111
+	  out PORTC, temp
+	  ldi temp, 0b11110000
+	  out PORTC, temp
+	  ldi temp, 0b00001111
+	  out PORTD, temp
+	  ldi temp, 0b11110000
+	  out PORTD, temp
+   rjmp LOOP                 
+      
+.EXIT 
