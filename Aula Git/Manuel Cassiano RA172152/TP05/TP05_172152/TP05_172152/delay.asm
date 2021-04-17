aa41e2080a42c8ef4beba037c22d9c1274d0051d
Cassiano013 <cassianomanuel13@gmail.com>
2021-04-17 18:43:40 -0300

    Rei dos projetinhos

---
+++ Aula Git/Manuel Cassiano RA172152/TP05/TP05_172152/TP05_172152/delay.asm
@@ -0,0 +1,64 @@
+delay_1s:
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        ret
+
+delay_100ms:
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        rcall delay_5ms
+        ret
+
+
+delay_5ms:
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        ret
+
+
+delay_1ms:
+        push r16;salva o valor em r16
+        ldi r16,99;determina o valor de r16, que seriam quantas vezes ele vai ter que fazer o loop
+        delay_1ms1: ;10us/loop
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+            nop; gasta 1 ciclo
+        dec r16
+        brne delay_1ms1
+        pop r16; retore value no r16
+        ret
