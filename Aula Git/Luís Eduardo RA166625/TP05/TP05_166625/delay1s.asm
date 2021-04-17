1cd722ee48ea14f3fd4745b1151dd20ac95d3996
lers145 <lers145@gmail.com>
2021-04-17 12:10:31 -0300

    TP05 Luis Eduardo

---
+++ Aula Git/Luís Eduardo RA166625/TP05/TP05_166625/delay1s.asm
@@ -0,0 +1,57 @@
+delay_1000ms:
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        rcall delay_100ms
+        ret
+
+delay_100ms:
+        rcall delay_10ms
+        rcall delay_10ms
+        rcall delay_10ms
+        rcall delay_10ms
+        rcall delay_10ms
+        rcall delay_10ms
+        rcall delay_10ms
+        rcall delay_10ms
+        rcall delay_10ms
+        rcall delay_10ms
+        ret
+
+delay_10ms:
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        rcall delay_1ms
+        ret
+
+delay_1ms:
+        push r16
+        ldi r16,99
+        delay_1ms1:
+            nop
+            nop
+            nop
+            nop
+            nop
+            nop
+            nop
+            nop
+            nop
+            nop
+        dec r16
+        brne delay_1ms1
+        pop r16
+        ret
