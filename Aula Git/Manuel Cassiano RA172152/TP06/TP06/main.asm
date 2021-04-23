93927e03b2d901420c9274927d990a63b239df61
Cassiano013 <cassianomanuel13@gmail.com>
2021-04-23 11:32:55 -0300

    TP06 Manuel

---
+++ Aula Git/Manuel Cassiano RA172152/TP06/TP06/main.asm
@@ -0,0 +1,83 @@
+;  /===============================================================================\
+; | Projeto:  TP06																|	
+; |																					|
+; | Autor: Manuel Cassiano Ferreira da Silva													|
+; | Idioma: NOME DA LÍNGUA EM QUE O PROGRAMA ESTÁ ESCRITO E							|
+; |			O NOME DO COMPILADOR USADO PARA COMPILÁ-LO QUANDO FOI TESTADO			|
+; | Solução: NOME DA SUA SOLUÇÃO ATMEL STUDIO 7 (Template neste exemplo)			|
+; |																					|
+; | Projetos: AssemblerTargetTeamplate												|
+; |																					|
+; | Para compilar: Explique como compilar este programa	                            |
+; |																					|
+; |Exemplo: Para compilar este código, deverá ter o compilador AVRASM2.exe (windows)|
+; | no prompt de comando (MS-DOS):													|
+; |																					|
+; |      avrasm2.exe -fI -o "Teamplate.hex"											|
+; |						 -m "Teamplate.map"											|
+; | 					 -l "Teamplate.lss"											|	
+; |						 -e "Teamplate.eep"											|  
+; |						 -S "Teamplate.tmp"											|	  
+; |						 -W+ie  -d "Debug\Teamplate.obj"							|
+; |						 main.asm													|
+; |																					|
+; | Software: NOME E TÍTULO DA CLASSE PARA QUE ESTE PROGRAMA FOI ESCRITO			|
+; |																					|
+; | Versão do SO: SEU NÚMERO DE VERSÃO DO PROGRAMA DE COMPUTADOR					|
+; |																					|
+; | Plataforma: SEU PROGRAMA DE COMPUTADOR											|
+; |																					|
+; | Instrutor: NOME DO INSTRUTOR DO SEU CURSO										|
+; |																					|
+; | Data de vencimento: DATA E HORA EM QUE ESTE PROGRAMA É / DEVIA SER				|
+; | SUBMETIDO																		|
+; |																					|
+; + --------------------------------------------------------------------------------+
+; |																					|
+; | Descrição: DESCREVA O PROBLEMA DE QUE ESTE PROGRAMA FOI ESCRITO RESOLVER.		|
+; |																					|
+; | Entrada: DESCREVA A ENTRADA QUE O PROGRAMA EXIGE.								|
+; |																					|
+; | Saída: DESCREVA A SAÍDA QUE O PROGRAMA PRODUZ.									|
+; |																					|
+; | Algoritmo: ESBOÇO DA ABORDAGEM USADA PELO PROGRAMA PARA RESOLVER O PROBLEMA.	|
+; |																					|
+; | Recursos necessários não incluídos: DESCREVA AQUI QUALQUER REQUISITO DE			|
+; | A tarefa que o programa não tenta resolver.										|
+; |																					|
+; | Erros conhecidos: SE O PROGRAMA NÃO FUNCIONAR CORRETAMENTE EM ALGUNS			|
+; |					  SITUAÇÕES, DESCREVA AS SITUAÇÕES E PROBLEMAS AQUI.			|
+; |																					|
+;  \===============================================================================/
+
+
+
+;.INCLUDE "m328Pdef.inc"
+.device atmega328P
+.nolist
+.list
+
+
+;=============================
+;Declaracoes
+
+
+.ORG 0x000 
+	
+	rjmp INICIO ; indica a posicao de memoria inicial.
+
+INICIO:
+
+		ldi r17, 0x37 
+		ldi r18, 45
+		ldi r19, 0b00111000
+		ldi r20, 0x2B
+		add r17, r18
+		add r19, r20
+		sub r17, r19
+		nop
+
+LOOP:
+	rjmp LOOP
+
+.EXIT
