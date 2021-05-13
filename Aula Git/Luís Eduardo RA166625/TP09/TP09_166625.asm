526e320d10e963e244f7845d1a06e532b8a80527
Luís Eduardo R <79853808+lers145@users.noreply.github.com>
2021-05-12 21:36:49 -0300

    TP09-166625

---
+++ Aula Git/Luís Eduardo RA166625/TP09/TP09_166625.asm
@@ -0,0 +1,27 @@
+;
+; TP9.asm
+;
+; Created: 11/05/2021 20:40:48
+; Author :Luís Eduardo
+;
+
+.nolist
+.list
+
+start:
+    rjmp INICIO
+    .def temp = r16
+
+    INICIO:
+        ldi temp, 0b11111111
+        out ddrb, r16
+
+    restart:
+        ldi r17, 0x00
+
+    main:
+        out portb, r17
+        inc r17
+        cpi r17,60
+        breq restart
+        rjmp main 
